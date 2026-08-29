0d5bdc7d66c7405a99cf9802422be239cf851ba5
Bangyogs365 <icsabellasatu@gmail.com>
2026-08-29 21:56:14 +0700

    Update MessageBubble.tsx

--- src/features/chat/components/MessageBubble.tsx
+++ src/features/chat/components/MessageBubble.tsx
@@ -1,5 +1,87 @@
-type Props = { text: string };
+interface Props{
 
-export default function MessageBubble({ text }: Props) {
-  return <div>{text}</div>;
+text:string;
+
+mine:boolean;
+
+read?:boolean;
+
+}
+
+
+export default function MessageBubble({
+text,
+mine,
+read
+}:Props){
+
+
+return (
+
+<div
+className={`
+flex
+${mine?"justify-end":"justify-start"}
+mb-3
+`}
+>
+
+
+<div
+className={`
+max-w-[75%]
+rounded-2xl
+px-4
+py-3
+
+${
+mine
+?
+"bg-blue-600 text-white"
+:
+"bg-zinc-900 text-gray-200"
+}
+
+`}
+>
+
+
+<p>
+{text}
+</p>
+
+
+{
+mine &&
+(
+<span
+className="
+text-xs
+opacity-70
+block
+text-right
+mt-1
+"
+>
+
+{
+read
+?
+"✓✓"
+:
+"✓"
+}
+
+</span>
+)
+}
+
+
+</div>
+
+
+</div>
+
+)
+
 }
